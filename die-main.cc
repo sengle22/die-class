c147219657ab0634aa1a4ce105c49ace0ed68d51
Shelby Engle <se352717@ohio.edu>
2020-08-27 21:25:05 -0400

    adding die-main.cc file

---
+++ die-main.cc
@@ -0,0 +1,26 @@
+/*
+* Author: Shelby Engle
+* Date: 8/27/2020
+* Description: executing die class
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include "die.h"
+
+using namespace std;
+
+//function prototypes
+int main() {
+    Die d;
+    cout << "Face value: "<< d << endl;
+    Die d12(12);
+    cout << "Face value: "<< d12 << endl;
+    d12.setNumSides(24);
+    for (size_t i = 0; i < 6; i++) {
+    cout << "Roll: " << d12.roll() << endl;
+    }
+
+return 0;
+}
